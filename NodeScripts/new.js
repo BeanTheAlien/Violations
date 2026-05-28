009347c09ac775a3bd031fed33829346b6ef755c
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-05-28 18:24:48 -0500

    intrinsic arg support, prop fixes

--- NodeScripts/new.js
+++ NodeScripts/new.js
@@ -1,14 +1,12 @@
 const fs = require("fs");
-const [,, name, extend = "Base"] = process.argv;
+const [,, name, extend = "Base", props = "{\n\t--\n}", argList = "name: string"] = process.argv;
 fs.writeFileSync(`src\\ReplicatedStorage\\Modules\\Shared\\${name}.luau`, `local ${extend} = require(script.Parent.${extend})
 local ${name} = {}
 ${name}.__index = ${name}
 setmetatable(${name}, ${extend})
-export type ${name} = ${extend}.${extend} & {
-    --
-}
-function ${name}.new(name: string): ${name}
-    local this = setmetatable(${extend}.new(name), ${name})
+export type ${name} = ${extend}.${extend} & ${props.split("\\n").join("\n")}
+function ${name}.new(${argList}): ${name}
+    local this = setmetatable(${extend}.new(${argList.split(",").map(x => x.trim()).map(x => x.split(":")[0]).join(", ")}), ${name})
     return this
 end
 return ${name}`);
