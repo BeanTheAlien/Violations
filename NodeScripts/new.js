e932303c0e09051e9b5295fde82077388d742134
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-05-28 18:39:17 -0500

    fix bugs, assignment

--- NodeScripts/new.js
+++ NodeScripts/new.js
@@ -1,12 +1,15 @@
 const fs = require("fs");
-const [,, name, extend = "Base", props = "{\n\t--\n}", argList = "name: string"] = process.argv;
+const [,, name, extend = "Base", props = "{\n\t--\n}", argListGeneric = null, useOnlyName = "false"] = process.argv;
+const argList = "name: string" + (argListGeneric != null ? ", " + argListGeneric : "");
+const argProp = argList.split(",").map(x => x.trim()).map(x => x.split(":")[0]);
 fs.writeFileSync(`src\\ReplicatedStorage\\Modules\\Shared\\${name}.luau`, `local ${extend} = require(script.Parent.${extend})
 local ${name} = {}
 ${name}.__index = ${name}
 setmetatable(${name}, ${extend})
-export type ${name} = ${extend}.${extend} & ${props.split("\\n").join("\n")}
+export type ${name} = ${extend}.${extend} & ${props.replaceAll("\\n", "\n").replaceAll("\\t", "\t")}
 function ${name}.new(${argList}): ${name}
-    local this = setmetatable(${extend}.new(${argList.split(",").map(x => x.trim()).map(x => x.split(":")[0]).join(", ")}), ${name})
+    local this = setmetatable(${extend}.new(${!Boolean(useOnlyName) ? argProp.join(", ") : "name"}), ${name})
+    ${argProp.length == 1 ? "" : argProp.slice(1).map(x => `this.${x} = ${x}`).join("\n\t")}
     return this
 end
 return ${name}`);
