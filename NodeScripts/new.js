52266c743c8da6507fb738d4bef89ee6673e2078
BeanTheAlien <ben.allen.goldstein@gmail.com>
2026-08-04 17:37:35 -0500

    structure changes

--- NodeScripts/new.js
+++ NodeScripts/new.js
@@ -1,12 +1,12 @@
 const fs = require("fs");
-const [,, name, tgPath = "", extend = "Base", props = "{\n\t--\n}", argListGeneric = null, useOnlyName = "false"] = process.argv;
+const [,, name, tgPath = "", extend = "Base", props = null, argListGeneric = null, useOnlyName = "false", useInSameFolder = "false"] = process.argv;
 const argList = "name: string" + (argListGeneric != null ? ", " + argListGeneric : "");
 const argProp = argList.split(",").map(x => x.trim()).map(x => x.split(":")[0]);
-fs.writeFileSync(`..\\src\\ReplicatedStorage\\Shared\\${tgPath}${name}.luau`, `local ${extend} = require(script.Parent.${"Parent.".repeat(tgPath.split("/").length - 1)}${extend})
+fs.writeFileSync(`..\\src\\ReplicatedStorage\\Shared\\${tgPath.replaceAll("/", "\\ ")}${name}.luau`, `local ${extend} = require(script.Parent.${useInSameFolder == "false" ? "Parent.".repeat(tgPath.split("/").length - 1) : ""}${extend})
 local ${name} = {}
 ${name}.__index = ${name}
 setmetatable(${name}, ${extend})
-export type ${name} = ${extend}.${extend} & ${props.replaceAll("\\n", "\n").replaceAll("\\t", "\t")}
+export type ${name} = ${extend}.${extend} & ${(props == "null" || props == null ? "{\n\t--\n}" : props).replaceAll("\\n", "\n").replaceAll("\\t", "\t")}
 function ${name}.new(${argList}): ${name}
     local this = setmetatable(${extend}.new(${!Boolean(useOnlyName) ? argProp.join(", ") : "name"}), ${name})
     ${argProp.length == 1 ? "" : argProp.slice(1).map(x => `this.${x} = ${x}`).join("\n\t")}
